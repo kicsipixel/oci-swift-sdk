7482c1c9006568b0fe35398a9faeff04986ebe30
Szabolcs Tóth <tsz@purzelbaum.hu>
2025-09-20 22:24:59 +0200

    getBucket, getNamespace and listBuckets can handle opc-client-request-id

--- Sources/OCIKit/services/TSzObjectStorage/TSzObjectStorageRouter.swift
+++ Sources/OCIKit/services/TSzObjectStorage/TSzObjectStorageRouter.swift
@@ -15,130 +15,124 @@
 import Foundation
 
 #if canImport(FoundationNetworking)
-    import FoundationNetworking
+  import FoundationNetworking
 #endif
 
 // HTTP methods
 public enum HTTPMethod: String {
-    case get = "GET"
-    case post = "POST"
-    case put = "PUT"
-    case delete = "DELETE"
+  case get = "GET"
+  case post = "POST"
+  case put = "PUT"
+  case delete = "DELETE"
 }
 
 // Protocol description
 public protocol API {
-    var path: String { get }
-    var method: HTTPMethod { get }
-    var queryItems: [URLQueryItem]? { get }
-    var headers: [String: String]? { get }
+  var path: String { get }
+  var method: HTTPMethod { get }
+  var queryItems: [URLQueryItem]? { get }
+  var headers: [String: String]? { get }
 }
 
 // API
 public enum ObjectStorageAPI: API {
-    /// Creates bucket
-    case createBucket(namespaceName: String, opcClientRequestId: String? = nil)
-    /// Gets bucket
-    case getBucket(namespaceName: String, bucketName: String)
-    /// Gets Namespace
-    case getNamespace(
-        compartmentId: String? = nil,
-        opcClientRequestId: String? = nil
-    )
-    /// Lists buckets
-    case listBuckets(namespaceName: String, compartmentId: String)
+  /// Creates bucket
+  case createBucket(namespaceName: String, opcClientRequestId: String? = nil)
+  /// Gets bucket
+  case getBucket(namespaceName: String, bucketName: String, opcClientRequestId: String? = nil)
+  /// Gets Namespace
+  case getNamespace(compartmentId: String? = nil, opcClientRequestId: String? = nil)
+  /// Lists buckets
+  case listBuckets(namespaceName: String, compartmentId: String, opcClientRequestId: String? = nil)
 
-    // Path
-    public var path: String {
-        switch self {
-        case .getNamespace:
-            return "/n"
-        case .createBucket(let namespaceName, _),
-            .listBuckets(let namespaceName, _):
-            return "/n/\(namespaceName)/b"
-        case .getBucket(let namespaceName, let bucketName):
-            return "/n/\(namespaceName)/b/\(bucketName)"
-        }
+  // Path
+  public var path: String {
+    switch self {
+    case .getNamespace:
+      return "/n"
+    case .createBucket(let namespaceName, _),
+      .listBuckets(let namespaceName, _, _):
+      return "/n/\(namespaceName)/b"
+    case .getBucket(let namespaceName, let bucketName, _):
+      return "/n/\(namespaceName)/b/\(bucketName)"
     }
+  }
 
-    // HTTPMethod
-    public var method: HTTPMethod {
-        switch self {
-        case .createBucket:
-            return .post
-        case .getNamespace,
-            .getBucket,
-            .listBuckets:
-            return .get
-        }
+  // HTTPMethod
+  public var method: HTTPMethod {
+    switch self {
+    case .createBucket:
+      return .post
+    case .getNamespace,
+      .getBucket,
+      .listBuckets:
+      return .get
     }
+  }
 
-    // QueryItems
-    public var queryItems: [URLQueryItem]? {
-        switch self {
-        case .createBucket,
-            .getBucket:
-            return nil
-        case .getNamespace(let compartmentId, _):
-            if let compartmentId {
-                return [
-                    URLQueryItem(name: "compartmentId", value: compartmentId)
-                ]
-            }
-            return nil
-        case .listBuckets(_, let compartmentId):
-            return [URLQueryItem(name: "compartmentId", value: compartmentId)]
-        }
+  // QueryItems
+  public var queryItems: [URLQueryItem]? {
+    switch self {
+    case .createBucket,
+      .getBucket:
+      return nil
+    case .getNamespace(let compartmentId, _):
+      if let compartmentId {
+        return [
+          URLQueryItem(name: "compartmentId", value: compartmentId)
+        ]
+      }
+      return nil
+    case .listBuckets(_, let compartmentId, _):
+      return [URLQueryItem(name: "compartmentId", value: compartmentId)]
     }
+  }
 
-    // Headers
-    public var headers: [String: String]? {
-        switch self {
-        case .createBucket(_, let opcClientRequestId),
-            .getNamespace(_, let opcClientRequestId):
-            if let opcClientRequestId {
-                return ["opc-client-request-id": opcClientRequestId]
-            }
-            return nil
-        case .getBucket,
-            .listBuckets:
-            return nil
-        }
+  // Headers
+  public var headers: [String: String]? {
+    switch self {
+    case .createBucket(_, let opcClientRequestId),
+      .getBucket(_, _, let opcClientRequestId),
+      .getNamespace(_, let opcClientRequestId),
+      .listBuckets(_, _, let opcClientRequestId):
+      if let opcClientRequestId {
+        return ["opc-client-request-id": opcClientRequestId]
+      }
+      return nil
     }
+  }
 }
 
 /// Build request from components defined in ObjectStorageAPIRouter
-public func buildRequest(objectStorageAPI: API, endpoint: URL) throws
-    -> URLRequest
-{
-    guard
-        var components = URLComponents(
-            url: endpoint,
-            resolvingAgainstBaseURL: false
-        )
-    else {
-        throw ObjectStorageError.invalidURL("Enpoint URL is invalid")
-    }
+public func buildRequest(objectStorageAPI: API, endpoint: URL) throws -> URLRequest {
+  guard
+    var components = URLComponents(
+      url: endpoint,
+      resolvingAgainstBaseURL: false
+    )
+  else {
+    throw ObjectStorageError.invalidURL("Enpoint URL is invalid")
+  }
 
-    // Build path
-    components.path = objectStorageAPI.path
+  // Build path
+  components.path = objectStorageAPI.path
 
-    // Add query items
-    components.queryItems = objectStorageAPI.queryItems
-    guard let url = components.url else {
-        throw ObjectStorageError.invalidURL("Could not construct final URL")
-    }
+  // Add query items
+  components.queryItems = objectStorageAPI.queryItems
+  guard let url = components.url else {
+    throw ObjectStorageError.invalidURL("Could not construct final URL")
+  }
 
-    // Build request
-    var request = URLRequest(url: url)
-    request.httpMethod = objectStorageAPI.method.rawValue
+  // Build request
+  var request = URLRequest(url: url)
+  request.httpMethod = objectStorageAPI.method.rawValue
 
-    // Add headers
-    objectStorageAPI.headers?.forEach { key, value in
-        request.addValue(value, forHTTPHeaderField: key)
-    }
-    request.setValue("application/json", forHTTPHeaderField: "accept")
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+  // Add headers
+  objectStorageAPI.headers?.forEach { key, value in
+    request.addValue(value, forHTTPHeaderField: key)
+  }
+  request.setValue("application/json", forHTTPHeaderField: "accept")
+  request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 
-    return request
+  return request
 }
