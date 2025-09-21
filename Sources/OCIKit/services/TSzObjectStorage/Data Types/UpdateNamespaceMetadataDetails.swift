508587765b9661dbc46bb3946c7d5432d9e0ac6f
Szabolcs Tóth <tsz@purzelbaum.hu>
2025-09-21 16:24:04 +0200

    UpdateNamespaceMetadataDetails model was added.

---
+++ Sources/OCIKit/services/TSzObjectStorage/Data Types/UpdateNamespaceMetadataDetails.swift
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the oci-swift-sdk open source project
+//
+// Copyright (c) 2025 Szabolcs Toth and the oci-swift-sdk project authors
+// Licensed under MIT License
+//
+// See LICENSE for license information
+// See CONTRIBUTORS.md for the list of oci-swift-sdk project authors
+//
+// SPDX-License-Identifier: MIT License
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+
+/// UpdateNamespaceMetadataDetails is used to update the NamespaceMetadata.
+public struct UpdateNamespaceMetadataDetails: Codable {
+  /// If the field is set, specifies the default compartment assignment for the Amazon S3 Compatibility API.
+  public let defaultS3CompartmentId: String
+  /// If the field is set, specifies the default compartment assignment for the Swift API.
+  public let defaultSwiftCompartmentId: String
+}
