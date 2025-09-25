773e099ce9b4e0f4b7012fe0410463aefc340f44
Szabolcs Tóth <tsz@purzelbaum.hu>
2025-09-25 18:41:32 +0200

    ReencryptObjectDetails and SSECustomerKeyDetails were added.

---
+++ Sources/OCIKit/services/TSzObjectStorage/Data Types/ReencryptObjectDetails.swift
@@ -0,0 +1,24 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the oci-swift-sdk open source project
+//
+// Copyright (c) 2025 Szabolcs Toth and the oci-swift-sdk project authors
+// Licensed under MIT License
+//
+// See LICENSE for license information
+// See CONTRIBUTORS.md for the list of oci-swift-sdk project authors
+//
+// SPDX-License-Identifier: MIT License
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+
+public struct ReencryptObjectDetails: Codable {
+  // The OCID of the master encryption key used to call the Vault service to re-encrypt the data encryption keys associated with the object and its chunks. If the kmsKeyId value is empty, whether null or an empty string, the API will perform re-encryption by using the kmsKeyId associated with the bucket or the master encryption key managed by Oracle, depending on the bucket encryption mechanism.
+  public let kmsKeyId: String?
+  // Specifies the details of the customer-provided encryption key (SSE-C) associated with an object.
+  public let sourceSseCustomerKey: SSECustomerKeyDetails?
+  // Specifies the details of the customer-provided encryption key (SSE-C) associated with an object.
+  public let sseCustomerKey: SSECustomerKeyDetails?
+}
