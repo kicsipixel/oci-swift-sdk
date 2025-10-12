4eb8b12bd8b4e8b4ad20878589fc64c131b8122d
Szabolcs Tóth <tsz@purzelbaum.hu>
2025-10-12 11:48:52 +0200

    WorkRequestSummary was added.

---
+++ Sources/OCIKit/services/ObjectStorage/Data Types/WorkRequestSummary.swift
@@ -0,0 +1,62 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the oci-swift-sdk open source project
+//
+// Copyright (c) 2025 Szabolcs Tóth and the oci-swift-sdk project authors
+// Licensed under MIT License
+//
+// See LICENSE for license information
+// See CONTRIBUTORS.md for the list of oci-swift-sdk project authors
+//
+// SPDX-License-Identifier: MIT License
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+
+public struct WorkRequestSummary: Codable {
+    /// The [OCID](https://docs.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the compartment that contains the work request. Work requests are scoped to the same compartment as the resource the work request affects.
+    /// If the work request affects multiple resources and those resources are not in the same compartment, the OCID of the primary resource is used. For example, you can copy an object in a bucket in one compartment to a bucket in another compartment. In this case, the OCID of the source compartment is used.
+    public let compartmentId: String?
+    /// The id of the work request.
+    public let id: String?
+    ///
+    public let operationType: OperationType?
+    /// Percentage of the work request completed.
+    public let percentComplete: Float?
+    public let resources: [WorkRequestResource]?
+    /// The status of the specified work request
+    public let status: WorkRequestStatus?
+    /// The raw string value of the time the work request was accepted.
+    private let timeAcceptedRaw: String
+    /// The raw string value of the time the work request was finished.
+    private let timeFinishedRaw: String
+    /// The raw string value of the time the work request was started.
+    private let timeStartedRaw: String
+    /// The parsed date and time the work request was accepted, based on RFC 3339 format.
+    public var timeAccepted: Date? {
+      Date.fromRFC3339(timeAcceptedRaw)
+    }
+
+    /// The parsed date and time the work request was finished, based on RFC 3339 format.
+    public var timeFinished: Date? {
+      Date.fromRFC3339(timeFinishedRaw)
+    }
+
+    /// The parsed date and time the work request was started, based on RFC 3339 format.
+    public var timeStarted: Date? {
+      Date.fromRFC3339(timeStartedRaw)
+    }
+
+    private enum CodingKeys: String, CodingKey {
+      case compartmentId
+      case id
+      case operationType
+      case percentComplete
+      case resources
+      case status
+      case timeAcceptedRaw = "timeAccepted"
+      case timeFinishedRaw = "timeFinished"
+      case timeStartedRaw = "timeStarted"
+    }
+}
