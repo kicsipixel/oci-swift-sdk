8abc2bb61d483e4b9ca97c2d782e07c24be47da4
Szabolcs Tóth <tsz@purzelbaum.hu>
2025-11-11 20:33:26 +0100

    init() was added back.

--- Sources/OCIKit/services/ObjectStorage/Models/BucketSummary.swift
+++ Sources/OCIKit/services/ObjectStorage/Models/BucketSummary.swift
@@ -12,6 +12,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+import Foundation
+
 public struct BucketSummary: Decodable {
   /// The compartment ID in which the bucket is authorized.
   public let compartmentId: String
@@ -55,5 +57,26 @@ public struct BucketSummary: Decodable {
     case name
     case namespace
     case timeCreatedRaw = "timeCreated"
+  }
+
+  /// Initializes a new `BucketSummary` instance.
+  public init(
+    compartmentId: String,
+    createdBy: String,
+    definedTags: [String: [String: String]]?,
+    etag: String,
+    freeformTags: [String: String]?,
+    name: String,
+    namespace: String,
+    timeCreatedRaw: String
+  ) {
+    self.compartmentId = compartmentId
+    self.createdBy = createdBy
+    self.definedTags = definedTags
+    self.etag = etag
+    self.freeformTags = freeformTags
+    self.name = name
+    self.namespace = namespace
+    self.timeCreatedRaw = timeCreatedRaw
   }
 }
