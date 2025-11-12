4d0a453f4b7cfc19c74a2190632b5db76b1f5911
Szabolcs Tóth <tsz@purzelbaum.hu>
2025-11-12 18:37:17 +0100

    PutObject downloads fiel from DropBox to temp folder then upload to ObjectStorage.

--- Tests/Linux/ObjectStorageTestOnLinux.swift
+++ Tests/Linux/ObjectStorageTestOnLinux.swift
@@ -51,35 +51,42 @@ struct ObjectStorageTestOnLinux {
     #expect(!namespace.isEmpty, "Namespace should not be empty")
   }
 
-@Test func putsObjectWithAPIKeySigner() async throws {
-  let regionId = try extractUserRegion(
-    from: ociConfigFilePath,
-    profile: ociProfileName
-  )
-  let region = Region.from(regionId: regionId ?? "") ?? .iad
-  let signer = try APIKeySigner(
-    configFilePath: ociConfigFilePath,
-    configName: ociProfileName
-  )
-  let sut = try ObjectStorageClient(region: region, signer: signer)
-  let fileToUploadPath = NSHomeDirectory() + "/Desktop/CleanShot 2025-11-03 at 16.23.55.png"
-  let fileToUploadURL = URL(fileURLWithPath: fileToUploadPath)
-  let data: Data = try Data(contentsOf: fileToUploadURL)
+    @Test func putsObjectWithAPIKeySigner() async throws {
+      let regionId = try extractUserRegion(
+        from: ociConfigFilePath,
+        profile: ociProfileName
+      )
+      let region = Region.from(regionId: regionId ?? "") ?? .iad
+      let signer = try APIKeySigner(
+        configFilePath: ociConfigFilePath,
+        configName: ociProfileName
+      )
+      let sut = try ObjectStorageClient(region: region, signer: signer)
 
-  do {
-    try await sut.putObject(
-      namespaceName: "frjfldcyl3la",
-      bucketName: "test_bucket_by_sdk",
-      objectName: "\(fileToUploadURL.lastPathComponent)",
-      putObjectBody: data
-    )
+      // Correct Dropbox direct download URL
+      guard let remoteURL = URL(string: "https://www.dropbox.com/scl/fi/eomsy2kohcjxqid7h1i7r/_.png?rlkey=z10i0f9jaum6cx5o2bpmjl5a2&dl=1") else {
+        throw URLError(.badURL)
+      }
 
-    #expect(true, "The operation should succeed")
-  }
-  catch {
-    Issue.record("putObject threw an error: \(error)")
-  }
-}
+      // Download to temporary location
+      let (downloadedData, _) = try await URLSession.shared.data(from: remoteURL)
+      let tempFileURL = FileManager.default.temporaryDirectory.appendingPathComponent("!@#$%^&*()_.png")
+      try downloadedData.write(to: tempFileURL)
+
+      do {
+        try await sut.putObject(
+          namespaceName: "frjfldcyl3la",
+          bucketName: "test_bucket_by_sdk",
+          objectName: "dropbox_image.png",
+          putObjectBody: downloadedData
+        )
+
+        #expect(true, "The operation should succeed")
+      } catch {
+        Issue.record("putObject threw an error: \(error)")
+      }
+    }
+
 
   // MARK: - Lists objects with Observable
   // Returning with `name`, `size`, `timeCreated` and `timeModified`
