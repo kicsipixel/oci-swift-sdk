95b11aa8f60b3593add60629b21b0b85345e210e
Szabolcs Tóth <tsz@purzelbaum.hu>
2025-11-28 21:07:08 +0100

    UnexpectedStatusCode error was introduced.

--- Sources/OCIKit/services/ObjectStorage/Models/ObjectStorageErrors.swift
+++ Sources/OCIKit/services/ObjectStorage/Models/ObjectStorageErrors.swift
@@ -14,29 +14,53 @@
 
 import Foundation
 
-/// ObjectStorage related errors
+/// ObjectStorage-related errors.
+/// Errors are recorded in logs and also thrown as exceptions.
+///
+/// Log format:
+/// `[ObjectStorage function name]` error code (HTTPResponse.statusCode): error message
+///
+/// Example:
+/// `[getObject]` error code (404): Object not found
+///
+/// Thrown error format (without the function name):
+/// Error (HTTPResponse.statusCode): error message
+///
+/// Example:
+/// Error (404): Object not found
 public enum ObjectStorageError: Error {
-  case missingRequiredParameter(String)
-  case invalidURL(String)
   case invalidResponse(String)
+  case invalidURL(String)
   case invalidUTF8
-  case jsonEncodingError(String)
   case jsonDecodingError(String)
+  case jsonEncodingError(String)
+  case missingRequiredParameter(String)
   case objectLengthMismatch(Int, Int)
   case objectMD5Mismatch(String, String)
+  case unexpectedStatusCode(Int, String)
 }
 
 extension ObjectStorageError: LocalizedError {
   public var errorDescription: String? {
     switch self {
-    case .missingRequiredParameter(let param): return "Missing required parameter \(param)"
-    case .invalidURL(let url): return "Provided URL is invalid: \(url)"
-    case .invalidResponse(let response): return "API returned an invalid reponse: \(response)"
-    case .invalidUTF8: return "Malformed UTF8 representation"
-    case .jsonEncodingError(let errorString): return "JSON encoding error: \(errorString)"
-    case .jsonDecodingError(let errorString): return "JSON decoding error: \(errorString)"
-    case .objectLengthMismatch(let actual, let original): return "Downloaded object length \(actual) does not match the original length reported by Object Storage \(original)"
-    case .objectMD5Mismatch(let actual, let original): return "Downloaded object MD5 \(actual) does not match the original MD5 reported by Object Storage \(original)"
+    case .invalidResponse(let response):
+      return "API returned an invalid response: \(response)"
+    case .invalidURL(let url):
+      return "Provided URL is invalid: \(url)"
+    case .invalidUTF8:
+      return "Malformed UTF8 representation"
+    case .jsonDecodingError(let errorString):
+      return "JSON decoding error: \(errorString)"
+    case .jsonEncodingError(let errorString):
+      return "JSON encoding error: \(errorString)"
+    case .missingRequiredParameter(let param):
+      return "Missing required parameter \(param)"
+    case .objectLengthMismatch(let actual, let original):
+      return "Downloaded object length \(actual) does not match the original length reported by Object Storage \(original)"
+    case .objectMD5Mismatch(let actual, let original):
+      return "Downloaded object MD5 \(actual) does not match the original MD5 reported by Object Storage \(original)"
+    case .unexpectedStatusCode(let code, let message):
+      return "Error (\(code)): \(message)"
     }
   }
 }
